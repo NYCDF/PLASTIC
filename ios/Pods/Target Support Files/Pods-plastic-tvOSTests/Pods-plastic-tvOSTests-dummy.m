23be0e153496d135e3d6fedc9a23c6998324a558
NiazAhmed151 <niaz151@gmail.com>
2019-09-19 16:03:55 -0400

    Remove Files

--- ios/Pods/Target Support Files/Pods-plastic-tvOSTests/Pods-plastic-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_plastic_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_plastic_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-plasticTests/Pods-plasticTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_plasticTests : NSObject
-@end
-@implementation PodsDummy_Pods_plasticTests
-@end
